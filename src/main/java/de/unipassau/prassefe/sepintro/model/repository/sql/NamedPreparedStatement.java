efadb3479115f80acf1493e36019ae999761e1e6
flx5 <me@flx5.com>
2017-03-04 17:36:36 +0100

    Reduce complexity of parse method

--- src/main/java/de/unipassau/prassefe/sepintro/model/repository/sql/NamedPreparedStatement.java
+++ src/main/java/de/unipassau/prassefe/sepintro/model/repository/sql/NamedPreparedStatement.java
@@ -31,48 +31,55 @@ class NamedPreparedStatement implements AutoCloseable {
 		boolean inQuotes = false;
 		char quoteChar = '"';
 
-		for (int i = 0; i < length; ++i) {
-			char c = query.charAt(i);
+		int pos = 0;
+		while (pos < length) {
+			char c = query.charAt(pos);
 
-			if (inQuotes) {
-				/*
-				 * don't combine these if statements as otherwise the else case
-				 * might be executed also it is inQuotes!
-				 */
-
-				if (c == quoteChar) {
+			if (c == '\'' || c == '"') {
+				if (!inQuotes) {
+					inQuotes = true;
+					quoteChar = c;
+				} else if (quoteChar == c) {
 					inQuotes = false;
 				}
-			} else if (c == '\'' || c == '"') {
-				inQuotes = true;
-				quoteChar = c;
-			} else if (c == ':' && i + 1 < length && Character.isJavaIdentifierStart(query.charAt(i + 1))) {
-
-				int j = i + 2;
-				while (j < length && Character.isJavaIdentifierPart(query.charAt(j))) {
-					// walk j to end of identifier
-					++j;
-				}
-
-				String name = query.substring(i + 1, j);
-
-				if (!paramMap.containsKey(name)) {
-					paramMap.put(name, new ArrayList<>());
-				}
-
-				paramMap.get(name).add(++index);
+			} else if (c == ':' && pos + 1 < length && Character.isJavaIdentifierStart(query.charAt(pos + 1))) {
+				String name = getWord(pos + 1, query);
+				registerName(name, ++index);
 
 				// Replace parameter with question mark
 				c = '?';
 
 				// skip past end of parameter
-				i += name.length();
+				pos += name.length();
 			}
 
 			parsedQuery.append(c);
+			++pos;
 		}
 
 		return parsedQuery.toString();
+	}
+
+	private void registerName(String name, int index) {
+		if (!paramMap.containsKey(name)) {
+			paramMap.put(name, new ArrayList<>());
+		}
+
+		paramMap.get(name).add(index);
+	}
+
+	private String getWord(int start, String text) {
+		int j = findEndOfWord(start, text);
+		return text.substring(start, j);
+	}
+
+	private int findEndOfWord(int start, String text) {
+		int position = start + 1;
+		while (position < text.length() && Character.isJavaIdentifierPart(text.charAt(position))) {
+			// walk j to end of word
+			++position;
+		}
+		return position;
 	}
 
 	public PreparedStatement getStatement() {
@@ -116,7 +123,7 @@ class NamedPreparedStatement implements AutoCloseable {
 			statement.setDate(index, value);
 		}
 	}
-	
+
 	public void setDate(String name, LocalDate value) throws SQLException {
 		setDate(name, Date.valueOf(value));
 	}
@@ -126,7 +133,7 @@ class NamedPreparedStatement implements AutoCloseable {
 			statement.setBytes(index, value);
 		}
 	}
-	
+
 	public void setTimestamp(String name, Timestamp value) throws SQLException {
 		for (int index : getIndexes(name)) {
 			statement.setTimestamp(index, value);
